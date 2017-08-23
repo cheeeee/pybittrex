8c61f5f67867adda6197b49586db53a6bd9aabfc
Matt Selph <matt.selph@gmail.com>
2017-08-23 11:52:45 -0400

    Cleaning up; fix syntax error

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Topic :: Software Development :: Libraries :: Python Modules'
+        'Topic :: Software Development :: Libraries :: Python Modules',
         'Programming Language :: Python :: 2',
         'Programming Language :: Pythno :: 2.7',
         'Programming Language :: Python :: 3',
@@ -27,7 +27,7 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'        
+        'Programming Language :: Python :: 3.6'
     ],
     keywords='bittrex bitcoin ethereum cryptocurrency trading',
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
